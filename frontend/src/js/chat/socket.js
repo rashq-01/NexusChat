0b8b1c88676e6d9f7230c9a63a07fc6d4194ec8e
rashq <rashq122@gmail.com>
2026-02-28 22:02:53 +0530

    adding ioToken

--- frontend/src/js/chat/socket.js
+++ frontend/src/js/chat/socket.js
@@ -1,10 +1,12 @@
 const currentUSER = JSON.parse(localStorage.getItem("userCredentials"));
-
+const token = localStorage.getItem("token");
+console.log(token);
 import { HOST } from "/src/js/HOSTS.js";
 
 const socket = io(HOST, {
   auth: {
     username: currentUSER.username,
+    token : token,
   },
 });
 
